8ce9356b3f18d125e385d76947b4e8f0ef8a57c9
Prashant Kumar <kumarprince2510@gmail.com>
2019-02-04 21:28:46 +0530

    added line of code for to free mediaplayer resources after completing of a audioplaying and or if user touch other song in listview to play

--- playAudioWithListViewAdapter.java
+++ playAudioWithListViewAdapter.java
@@ -10,6 +10,17 @@ import java.util.ArrayList;
 
 public class playAudioWithListViewAdapterAtivityextends AppCompatActivity {
     private MediaPlayer mMediaPlayer;
+     /**
+     * This listener gets triggered when the {@link MediaPlayer} has completed
+     * playing the audio file.
+     */
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mp) {
+            releaseMediaPlayer();
+        }
+    };
+
     
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +35,27 @@ public class playAudioWithListViewAdapterAtivityextends AppCompatActivity {
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 mMediaPlayer = MediaPlayer.create(playAudioWithListViewAdapterAtivity.this, R.raw.songname);
                 mMediaPlayer.start();
+                // Setup a listener on the media player, so that we can stop and release the
+                // media player once the sound has finished playing.
+                mMediaPlayer.setOnCompletionListener(mCompletionListener);
             }
         });
+           
     }
+/**
+ * Clean up the media player by releasing its resources.
+ */
+private void releaseMediaPlayer() {
+    // If the media player is not null, then it may be currently playing a sound.
+    if (mMediaPlayer != null) {
+        // Regardless of the current state of the media player, release its resources
+        // because we no longer need it.
+        mMediaPlayer.release();
+
+        // Set the media player back to null. For our code, we've decided that
+        // setting the media player to null is an easy way to tell that the media player
+        // is not configured to play an audio file at the moment.
+        mMediaPlayer = null;
+    }
+}
 }
